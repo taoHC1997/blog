ce12fa9269112c46738f32098e3f8a9a37f640e8
taoHC1997 <55778123+taoHC1997@users.noreply.github.com>
2022-06-24 12:46:14 +0800

    svg 图片服务创建

--- demo/009 Nest/nest-demo/src/image/svg/svg.controller.ts
+++ demo/009 Nest/nest-demo/src/image/svg/svg.controller.ts
@@ -1,13 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Header } from '@nestjs/common';
 import { SvgService } from './svg.service';
 
 @Controller('image/svg')
 export class SvgController {
   constructor(private readonly svgService: SvgService) {}
 
-  // 测试
-  @Get('test')
-  test() {
-    return this.svgService.test();
+  @Get(['/', '/help'])
+  usage() {
+    return `
+    用法（图片大小/图片背景颜色/图片字体颜色/图片文字）：<br>
+    1. <a href="/image/svg/400x300/ccc/111">/image/svg/400x300/ccc/111</a><br>
+    2. <a href="/image/svg/400">/image/svg/400</a><br>
+    `;
+  }
+
+  /**
+   * 返回 svg 图片
+   * @param size 图片大小（宽x高）；默认 400 300
+   * @param backgroundColor 图片背景颜色；默认 ccc
+   * @param fontColor 图片字体颜色；默认 111
+   * @param text 图片文字，居中显示
+   * @returns svg 图片
+   */
+  @Get([
+    ':size',
+    ':size/:backgroundColor',
+    ':size/:backgroundColor/:fontColor',
+    ':size/:backgroundColor/:fontColor/:text',
+  ])
+  @Header('Content-Type', 'image/svg+xml')
+  generateSvg(
+    @Param('size') size: string,
+    @Param('backgroundColor') backgroundColor?: string,
+    @Param('fontColor') fontColor?: string,
+    @Param('text') text?: string,
+  ) {
+    const size_regex = /^(\d{1,5})(?:x(\d{1,5})$|$)/;
+    // 因为 url 中 # 有特殊意义，故 url 去掉了 #
+    const color_regex = /^([a-fA-F\d]{6}|[a-fA-F\d]{3})$/;
+    const _size = size_regex.exec(size) || ['400x300', '400', '300'];
+    if (!_size[2]) {
+      _size[2] = _size[1];
+    }
+    const _backgroundColor = color_regex.test(backgroundColor)
+      ? backgroundColor
+      : 'ccc';
+    const _fontColor = color_regex.test(fontColor) ? fontColor : '111';
+    const _text = text || `${_size[1]}x${_size[2]}`;
+    return this.svgService.generateSvg(
+      parseInt(_size[1]),
+      parseInt(_size[2]),
+      _backgroundColor,
+      _fontColor,
+      _text,
+    );
   }
 }
--- demo/009 Nest/nest-demo/src/image/svg/svg.service.ts
+++ demo/009 Nest/nest-demo/src/image/svg/svg.service.ts
@@ -6,4 +6,51 @@ export class SvgService {
   test(): string {
     return 'test';
   }
+
+  /**
+   * 生成 svg 图片
+   * @param width 宽
+   * @param height 高
+   * @param backgroundColor 背景颜色
+   * @param fontColor 字体颜色
+   * @param text 文字
+   * @returns svg 图片
+   */
+  generateSvg(
+    width: number,
+    height: number,
+    backgroundColor: string,
+    fontColor: string,
+    text: string,
+  ) {
+    const fontSize = this.calculateScaledFontSize(width, height);
+    return (
+      '' +
+      `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none">
+      <rect width="${width}" height="${height}" fill="#${backgroundColor}"/>
+      <text text-anchor="middle" x="${width / 2}" y="${height / 2}"
+        style="fill:#${fontColor};font-weight:bold;font-size:${fontSize}px;font-family:Arial,Helvetica,sans-serif;dominant-baseline:central">
+        ${text}
+      </text>
+    </svg>`
+    );
+  }
+
+  /**
+   * 粗略计算字体大小
+   * @param width 图片宽
+   * @param height 图片高
+   * @returns 字体大小
+   */
+  calculateScaledFontSize(width: number, height: number): number {
+    return Math.round(
+      Math.max(
+        12,
+        Math.min(
+          Math.min(width, height) * 0.75,
+          (0.75 * Math.max(width, height)) / 12,
+        ),
+      ),
+    );
+  }
 }
